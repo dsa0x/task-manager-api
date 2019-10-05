c9042f66e974032b219b42d4779ab2d6702f83aa
Dare Samsondeen <dsamsondeen@gmail.com>
2019-10-05 21:45:02 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
-const app = express();
+const app = require("./app");
 const port = process.env.PORT;
-
-//express middleware
-/* app.use((req, res, next) => {
-  if (req.method === "GET") {
-    res.send("GET requests are disabled");
-  } else {
-    next();
-  }
-
-  next();
-}); */
-//Parse incoming json
-app.use(express.json());
-
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
